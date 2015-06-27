b3d491b29681b68a45c3802d8fbde60db93076b8
NilayKulkarni <typhoscientic@gmail.com>
2015-06-27 19:49:50 +0530

    K5_Test2

--- leaf-demo.js
+++ leaf-demo.js
@@ -13,6 +13,59 @@ L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
 
 var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' );
 
+//var interval = setInterval(function () {getLast()}, 1000);
+
+var cred = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'us-east-1:08e41de7-9cb0-40d6-9f04-6f8956ed25bb',
+    });
+    AWS.config.credentials = cred;
+    AWS.config.region = 'us-east-1';
+    var db = new AWS.DynamoDB();
+    
+
+function getLast(){
+        var marams = {
+            TableName : 'test_gate1',
+            IndexName : 'Plotted-n-index',
+            KeyConditions :
+            {
+                "Plotted" :
+                {
+                    "AttributeValueList" : [
+                        {
+                            "N" : '1'
+                        }
+                    ],
+                    "ComparisonOperator" : "EQ"
+                }
+            },
+            ScanIndexForward : false
+        }
+        
+        db.query(marams, function(err, data){
+            if(err) console.log(err, err.stack);
+            else{
+                inCnt = data.Items[0].In.N;
+                console.log(inCnt);
+            }
+        });
+    mapFunc();
+    }
+
+    
+mapFunc = function(){
+    for(var i=0; i < markers.length ; i++){
+        markers[i].cnt = inCnt;
+    }
+    for ( var i=0; i < markers.length; ++i ) 
+    {
+        markers[i].cnt = inCnt;
+       L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
+          .bindPopup( '<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>' + '<p>' + markers[i].cnt + '</p>' )
+          .addTo( map );
+    }
+}
+
 var myIcon = L.icon({
     iconUrl: myURL + 'images/pin24.png',
     iconRetinaUrl: myURL + 'images/pin48.png',
@@ -21,9 +74,3 @@ var myIcon = L.icon({
     popupAnchor: [0, -14]
 });
 
-for ( var i=0; i < markers.length; ++i ) 
-{
-   L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
-      .bindPopup( '<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>' )
-      .addTo( map );
-}
